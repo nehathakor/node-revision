c2d7e1f7f59b72e6e8de531c4ea128d3ef12c2b6
Neha Thakor <neha.k.thakor@gmail.com>
2024-02-28 18:27:26 +0530

    second commit

--- Folder1/index.js
+++ Folder1/index.js
@@ -1,13 +1,24 @@
 const http = require('http');
 const fs = require("fs");
+const url = require("url");
 
-const myServer = http.createServer((req,res)=>{
-    const log = `${Date.now()}: New Req Rec.`
+const heyServer = http.createServer((req,res)=>{
+    const log = `${Date.now()}: ${req.url} New request received\n`;
     fs.appendFile("./log.txt",log,(err,data)=>{
-        res.end("Hello From Server")
+        const myUrl = url.parse(req.url,true);
+        console.log(myUrl);
+        if(req.url === '/favicon.ico') return res.end();
+        switch(myUrl.pathname){
+            case '/': res.end("HomePage");
+            break;
+            case '/about': 
+            const username = myUrl.query.myname;
+            res.end(`Hi, ${username}`);
+            break;
+            default:
+                res.end('404 not found');
+        }
     });
 });
 
-myServer.listen(8000,()=>{
-    console.log("Server started");
-});
+heyServer.listen(8000);
