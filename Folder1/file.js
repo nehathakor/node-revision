6b79ffb808193b35d38ea9e17641c10624ff0ed3
Neha Thakor <neha.k.thakor@gmail.com>
2024-02-28 14:24:44 +0530

    second commit

--- Folder1/file.js
+++ Folder1/file.js
@@ -1,1 +1,23 @@
+
 const fs = require("fs");
+
+//fs.writeFileSync("./text.txt","Hii neha");
+
+// const result = fs.readFile("./text.txt","utf8")
+// console.log(result)
+
+// fs.readFile("./text.txt","utf-8",(err,result)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log(result);
+//     }
+// });
+
+// fs.appendFileSync("./text.txt",`Hey there\n`);
+
+// fs.copyFileSync("./text.txt","./copy.txt");
+
+// fs.unlinkSync("./copy.txt");
+
+fs.mkdirSync("mydoc");
